44f96ab6d04e5e28aa7a3994f125982e46029ed9
Maximilian Kern <maxkernw@gmail.com>
2018-10-03 11:34:32 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'artprtln';
+  data: any;
+  readonly url = '/v2/sightings?dateFrom=2018-10-01&dateTo=2018-10-02';
+  readonly header = {'Ocp-Apim-Subscription-Key': 'a1ea858711ac4f96a6f703c7b237e35f'};
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit(): void {
+
+    this.httpService.get<any>(this.url,new HttpHeaders(this.header))
+          .subscribe(response => {
+            this.data = response;
+          }, error => console.error(error))
+  }
 }
---
+++ src/app/http.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { IHttpService } from './http.service.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService implements IHttpService {
+
+  private baseUrl = environment.url;
+
+  constructor(private http: HttpClient) { }
+
+  get<T>(path: string, headers?: HttpHeaders): Observable<T> {
+    return this.http.get<T>(`${this.baseUrl}/${path}`, {
+      headers: headers
+    });
+  }
+}
